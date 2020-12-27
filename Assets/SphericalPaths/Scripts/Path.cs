96d151555ec42f2d2800c25198cd7fcd68a97700
omaddam <omaddam@gmail.com>
2020-12-27 13:28:10 -0700

    Extracted the line renderer on awake.

--- Assets/SphericalPaths/Scripts/Path.cs
+++ Assets/SphericalPaths/Scripts/Path.cs
@@ -5,5 +5,26 @@ namespace SphericalPaths
     public class Path : MonoBehaviour
     {
 
+        #region Initialization
+
+        /// <summary>
+        /// Executes once on awake.
+        /// </summary>
+        private void Awake()
+        {
+            LineRenderer = GetComponent<LineRenderer>();
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// References the line renderer attached to this object that displays the path.
+        /// </summary>
+        private LineRenderer LineRenderer;
+
+        #endregion
+
     }
 }
