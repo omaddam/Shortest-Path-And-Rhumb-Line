ed18251ede6fbe98e1f2b5fd936f3e709efe5600
omaddam <omaddam@gmail.com>
2020-12-27 04:55:34 -0700

    Created an empty script and attached to the point prefab.

---
+++ Assets/SphericalPaths/Scripts/Point.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SphericalPaths
+{
+    public class Point : MonoBehaviour
+    {
+
+        #region Initialization
+
+
+
+        #endregion
+
+        #region Fields/Properties
+
+
+
+        #endregion
+
+    }
+}
