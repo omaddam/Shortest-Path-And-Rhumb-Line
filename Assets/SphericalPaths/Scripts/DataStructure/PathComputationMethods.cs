a20fb926aa47d0502c63716d90a5944e1bf7a8bd
omaddam <omaddam@gmail.com>
2020-12-27 02:53:54 -0700

    Created the method that generates the rhumb path.

--- Assets/SphericalPaths/Scripts/DataStructure/PathComputationMethods.cs
+++ Assets/SphericalPaths/Scripts/DataStructure/PathComputationMethods.cs
@@ -1,21 +1,44 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace SphericalPaths.DataStructure
 {
     public static class PathComputationMethods
     {
 
-		#region Common Methods
+        #region Constants
 
-		/// <summary>
-		/// Converts an angle from degree to radian.
-		/// </summary>
-		/// <returns>Angle in radian.</returns>
-		/// <param name="degree">Angle in degree.</param>
-		private static double ConvertFromDegreeToRadian(double degree)
+        /// <summary>
+        /// The default number of coordinates required to form a path.
+        /// </summary>
+        private const int RHUMB_PATH_SEGMENTS_COUNT = 15;
+
+        #endregion
+        
+        #region Conversion Methods
+
+        /// <summary>
+        /// Converts an angle from degree to radian.
+        /// </summary>
+        /// <returns>Angle in radian.</returns>
+        /// <param name="degree">Angle in degree.</param>
+        private static double ConvertFromDegreeToRadian(double degree)
 		{
 			return Math.PI * degree / 180.0;
 		}
+
+        /// <summary>
+        /// Converts cartesian coordinates from degrees to radian.
+        /// </summary>
+        private static Vector2 ToRadian(this Vector2 cartesian)
+        {
+            return new Vector2
+            (
+                (float)ConvertFromDegreeToRadian(cartesian.x),
+                (float)ConvertFromDegreeToRadian(cartesian.y)
+            );
+        }
 
 		/// <summary>
 		/// Converts an angle from radian to degree.
@@ -27,7 +50,108 @@ namespace SphericalPaths.DataStructure
 			return radian * (180.0 / Math.PI);
 		}
 
-		#endregion
+        /// <summary>
+        /// Converts cartesian coordinates from radian to degrees.
+        /// </summary>
+        private static Vector2 ToDegree(this Vector2 cartesian)
+        {
+            return new Vector2
+            (
+                (float)ConvertFromRadianToDegree(cartesian.x),
+                (float)ConvertFromRadianToDegree(cartesian.y)
+            );
+        }
 
-	}
+        #endregion
+
+        #region Rhumb Path Methods
+
+        /// <summary>
+        /// Generates the path between the two coordinates using rhumb line.
+        /// </summary>
+        /// <param name="start">The first coordinates in the path.</param>
+        /// <param name="end">The last coordinates in the path.</param>
+        /// <param name="segmentsCount">The default number of coordinates required to form the path.</param>
+		public static Path GetRhumbPath(this Coordinates start, Coordinates end, 
+            int segmentsCount = RHUMB_PATH_SEGMENTS_COUNT)
+        {
+            return new Path(GetRhumbLine(start, end, segmentsCount));
+        }
+
+        /// <summary>
+        /// Computes the coordinates between the two points using rhumb line.
+        /// </summary>
+        /// <param name="start">The first coordinates in the path.</param>
+        /// <param name="end">The last coordinates in the path.</param>
+        /// <param name="segmentsCount">The default number of coordinates required to form the path.</param>
+        private static List<Coordinates> GetRhumbLine(Coordinates start, Coordinates end,
+            int segmentsCount)
+        {
+            // Compute midpoint coordinates
+            Coordinates midCoordinates = GetRhumMidPoint(start, end);
+
+            // Decrement segments count
+            segmentsCount--;
+
+            // Compute left coordinates
+            List<Coordinates> leftCoordinates = segmentsCount > 0 ? null
+                : GetRhumbLine(start, midCoordinates, segmentsCount);
+
+            // Compute right coordinates
+            List<Coordinates> rightCoordinates = segmentsCount > 0 ? null
+                : GetRhumbLine(midCoordinates, end, segmentsCount);
+
+            // Set coordinates
+            List<Coordinates> coordinates = new List<Coordinates>();
+            if (leftCoordinates != null)
+                coordinates.AddRange(leftCoordinates);
+            coordinates.Add(midCoordinates);
+            if (rightCoordinates != null)
+                coordinates.AddRange(rightCoordinates);
+            return coordinates;
+        }
+
+        /// <summary>
+        /// Computes the midpoint between two coordinates on a rhumb line.
+        /// </summary>
+        private static Coordinates GetRhumMidPoint(Coordinates start, Coordinates end)
+        {
+            // Convert from degree to radian
+            Vector2 startRadian = start.CartesianCoordinates.ToRadian();
+            Vector2 endRadian = end.CartesianCoordinates.ToRadian();
+
+            // Crossing anti-meridian
+            if (Math.Abs(endRadian.x - startRadian.x) > Math.PI)
+                startRadian.x += (float)(2 * Math.PI);
+
+            // Compute midpoint latitude
+            float midLat = (startRadian.y + endRadian.y) / 2f;
+
+            // Compute midpoint longitude
+            double f1 = Math.Tan(Math.PI / 4 + startRadian.y / 2);
+            double f2 = Math.Tan(Math.PI / 4 + endRadian.y / 2);
+            double f3 = Math.Tan(Math.PI / 4 + midLat / 2);
+            float midLong = 
+                (float)
+                ((
+                    (endRadian.x - startRadian.x) * Math.Log(f3) 
+                    +
+                    startRadian.x * Math.Log(f2) 
+                    -
+                    endRadian.x * Math.Log(f1)
+                ) / Math.Log(f2 / f1));
+
+            if (double.IsInfinity(midLong) || double.IsNaN(midLong)) // parallel of latitude
+                midLong = (startRadian.x + endRadian.x) / 2;
+
+            midLong = (float)((midLong + 3 * Math.PI) % (2 * Math.PI) - Math.PI); // normalise to -180..+180°
+
+            // Compute midpoint
+            Vector2 midRadian = new Vector2(midLong, midLat);
+            return new Coordinates(midRadian.ToDegree(), start.Radius, start.Width);
+        }
+
+        #endregion
+
+    }
 }
