f791ea1f11ad746c78e92443de470e200a31ac5d
omaddam <omaddam@gmail.com>
2020-12-27 04:09:46 -0700

    Computed the sphere radius on awake.

--- Assets/SphericalPaths/Scripts/Sphere.cs
+++ Assets/SphericalPaths/Scripts/Sphere.cs
@@ -7,7 +7,42 @@ namespace SphericalPaths
     public class Sphere : MonoBehaviour
     {
 
+        #region Initialization
 
+        /// <summary>
+        /// Executes once on start.
+        /// </summary>
+        private void Awake()
+        {
+            // Extract the radius of thye sphere
+            _Radius = SphereParent.transform.localScale.x / 2f;
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        [Header("Sphere")]
+
+        /// <summary>
+        /// References the gameobject displays the 3d sphere.
+        /// </summary>
+        [Tooltip("References the gameobject displays the 3d sphere.")]
+        public GameObject SphereParent;
+
+        /// <summary>
+        /// The radius of the sphere.
+        /// </summary>
+        [Tooltip("The radius of the sphere. Geenrated on awake.")]
+        [SerializeField]
+        private float _Radius;
+
+        /// <summary>
+        /// The radius of the sphere.
+        /// </summary>
+        public float Radius { get { return _Radius; } }
+
+        #endregion
 
     }
 }
