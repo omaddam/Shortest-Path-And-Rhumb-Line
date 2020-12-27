98be38b5222da02ee1e2cfdf0dddd93edb814edc
omaddam <omaddam@gmail.com>
2020-12-27 04:58:44 -0700

    Added a method to initialize the point.

--- Assets/SphericalPaths/Scripts/Point.cs
+++ Assets/SphericalPaths/Scripts/Point.cs
@@ -1,3 +1,4 @@
+using SphericalPaths.DataStructure;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,24 @@ namespace SphericalPaths
 
         #region Initialization
 
-
+        /// <summary>
+        /// Initializes the point.
+        /// </summary>
+        public void Initialize(Coordinates coordinates)
+        {
+            Coordinates = coordinates;
+        }
 
         #endregion
 
         #region Fields/Properties
 
-
+        /// <summary>
+        /// Coordinates presented by this point.
+        /// </summary>
+        [Tooltip("An Coordinates presented by this point.")]
+        [SerializeField]
+        private Coordinates Coordinates;
 
         #endregion
 
