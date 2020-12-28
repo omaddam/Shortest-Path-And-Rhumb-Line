c5da3b5eee93f4845c39a9c9367211e2c086d920
omaddam <omaddam@gmail.com>
2020-12-27 21:42:33 -0700

    Displayed application version.

--- Assets/App/Scripts/IntroductionSceneManager.cs
+++ Assets/App/Scripts/IntroductionSceneManager.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroductionSceneManager : MonoBehaviour
 {
 
+    #region Initialization
 
+    /// <summary>
+    /// Execute once on awake.
+    /// </summary>
+    private void Awake()
+    {
+        // Display version
+        VersionText.text = string.Format("Version: {0}", Application.version);
+    }
+
+    #endregion
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// References the text UI element that displays the verison of the application.
+    /// </summary>
+    [Tooltip("References the text UI element that displays the verison of the application.")]
+    [SerializeField]
+    private Text VersionText;
+
+    #endregion
 
 }
