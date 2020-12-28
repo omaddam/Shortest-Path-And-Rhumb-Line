200ee23c985d63aa80daf80d90a9cd048df0d266
omaddam <omaddam@gmail.com>
2020-12-27 18:10:20 -0700

    Referenced the 2d plane parent.

--- Assets/SphericalPaths/Scripts/Plane.cs
+++ Assets/SphericalPaths/Scripts/Plane.cs
@@ -7,7 +7,18 @@ namespace SphericalPaths
     public class Plane : MonoBehaviour
     {
 
+        #region Fields/Properties
 
+        [Header("Plane")]
+
+        /// <summary>
+        /// References the gameobject displays the 3d sphere.
+        /// </summary>
+        [Tooltip("References the gameobject displays the 2d plane.")]
+        [SerializeField]
+        private GameObject PlaneParent;
+
+        #endregion
 
     }
 }
