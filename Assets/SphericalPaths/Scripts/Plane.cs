cd6ed539644a60ba69add75a624540550228c8de
omaddam <omaddam@gmail.com>
2020-12-27 18:16:28 -0700

    Added the option to set a texture on the plane.

--- Assets/SphericalPaths/Scripts/Plane.cs
+++ Assets/SphericalPaths/Scripts/Plane.cs
@@ -7,6 +7,28 @@ namespace SphericalPaths
     public class Plane : MonoBehaviour
     {
 
+        #region Initialization
+
+        /// <summary>
+        /// Executes once on start.
+        /// </summary>
+        private void Awake()
+        {
+            // Extract the width of the plane
+            _Width = PlaneParent.transform.localScale.x * 10;
+
+            // Extract the material used on the plane
+            MeshRenderer renderer = PlaneParent.GetComponent<MeshRenderer>();
+            PlaneMaterial = new Material(renderer.material);
+            renderer.material = PlaneMaterial;
+
+            // Apply texture
+            if (Texture != null)
+                PlaneMaterial.SetTexture("_MainTex", Texture);
+        }
+
+        #endregion
+
         #region Fields/Properties
 
         [Header("Plane")]
@@ -17,6 +39,30 @@ namespace SphericalPaths
         [Tooltip("References the gameobject displays the 2d plane.")]
         [SerializeField]
         private GameObject PlaneParent;
+
+        /// <summary>
+        /// The width of the plane.
+        /// </summary>
+        [Tooltip("The width of the plane. Generated on awake.")]
+        [SerializeField]
+        private float _Width;
+
+        /// <summary>
+        /// The width of the plane.
+        /// </summary>
+        public float Width { get { return _Width; } }
+
+        /// <summary>
+        /// References the material used on the plane.
+        /// </summary>
+        private Material PlaneMaterial;
+
+        /// <summary>
+        /// An optional texture that will be applied to the plane.
+        /// </summary>
+        [Tooltip("An optional texture that will be applied to the plane.")]
+        [SerializeField]
+        private Texture Texture;
 
         #endregion
 
