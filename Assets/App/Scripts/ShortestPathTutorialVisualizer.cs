aacba2842d8d52a3a9562043c45a8c75e2e7009c
omaddam <omaddam@gmail.com>
2020-12-30 03:49:39 -0700

    Created a tutorial visualizer script.

---
+++ Assets/App/Scripts/ShortestPathTutorialVisualizer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ShortestPathTutorialVisualizer : MonoBehaviour
+{
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// References the sphere in the scene.
+    /// </summary>
+    public SphericalPaths.Sphere Sphere;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Visualizes the tutorial based on the provided step.
+    /// </summary>
+    public void Display(int step)
+    {
+
+    }
+
+    #endregion
+
+}
