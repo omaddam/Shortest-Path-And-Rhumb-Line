107f5ae26f5423d5d55a11f593c352766709cb3e
omaddam <omaddam@gmail.com>
2020-12-30 04:04:37 -0700

    Implemented the first step.

--- Assets/App/Scripts/ShortestPathTutorialVisualizer.cs
+++ Assets/App/Scripts/ShortestPathTutorialVisualizer.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShortestPathTutorialVisualizer : MonoBehaviour
 {
+
+    #region Constants
+
+    /// <summary>
+    /// The color of the straight line crossing the sphere to connect the start and end points.
+    /// </summary>
+    private static readonly Color STRAIGHT_LINE_COLOR = Color.magenta;
+
+    #endregion
 
     #region Fields/Properties
 
@@ -25,6 +35,61 @@ public class ShortestPathTutorialVisualizer : MonoBehaviour
     /// Visualizes the tutorial based on the provided step.
     /// </summary>
     public void Display(int step)
+    {
+        // Clear all paths
+        Sphere.ClearPaths();
+
+        // Display steps
+        if (step >= 1)
+            DisplayStep1();
+        else if (step >= 2)
+            DisplayStep2();
+        else if (step >= 3)
+            DisplayStep3();
+        else if (step >= 4)
+            DisplayStep4();
+    }
+
+    /// <summary>
+    /// Displays the first step in the tutorial.
+    /// </summary>
+    private void DisplayStep1()
+    {
+        // Display straight line crossing the sphere between the start and end points
+        Sphere.DisplayPaths
+        (
+            new SphericalPaths.DataStructure.Path
+            (
+                new List<SphericalPaths.DataStructure.Coordinates>
+                {
+                    PathsScriptableObject.StartCoordinates,
+                    PathsScriptableObject.EndCoordinates
+                }
+            ),
+            STRAIGHT_LINE_COLOR
+        );
+    }
+
+    /// <summary>
+    /// Displays the second step in the tutorial.
+    /// </summary>
+    private void DisplayStep2()
+    {
+
+    }
+
+    /// <summary>
+    /// Displays the thjird step in the tutorial.
+    /// </summary>
+    private void DisplayStep3()
+    {
+
+    }
+
+    /// <summary>
+    /// Displays the fourth step in the tutorial.
+    /// </summary>
+    private void DisplayStep4()
     {
 
     }
