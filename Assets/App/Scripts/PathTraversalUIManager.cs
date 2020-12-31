da287331b55149678b9d265cfcb7c12dd39b2492
omaddam <omaddam@gmail.com>
2020-12-31 00:16:06 -0700

    Referenced the panel UI elements.

--- Assets/App/Scripts/PathTraversalUIManager.cs
+++ Assets/App/Scripts/PathTraversalUIManager.cs
@@ -1,10 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PathTraversalUIManager : MonoBehaviour
 {
 
+    #region Fields/Properties
 
+    /// <summary>
+    /// References the planel that holds the teaser.
+    /// </summary>
+    [Tooltip("References the planel that holds the teaser.")]
+    [SerializeField]
+    private GameObject TeaserPanel;
+
+    /// <summary>
+    /// References the planel that holds the compass.
+    /// </summary>
+    [Tooltip("References the planel that holds the compass.")]
+    [SerializeField]
+    private GameObject CompassPanel;
+
+    /// <summary>
+    /// References the start button.
+    /// </summary>
+    [Tooltip("References the shortest path start button.")]
+    [SerializeField]
+    private Button StartShortestPathButton;
+
+    /// <summary>
+    /// References the start button.
+    /// </summary>
+    [Tooltip("References the rhumb path start button.")]
+    [SerializeField]
+    private Button StartRhumbPathButton;
+
+    /// <summary>
+    /// References the cancel button.
+    /// </summary>
+    [Tooltip("References the cancel button.")]
+    [SerializeField]
+    private Button CancelButton;
+
+    #endregion
 
 }
