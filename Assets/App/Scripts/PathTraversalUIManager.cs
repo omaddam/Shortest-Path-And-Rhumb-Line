a27d21baaedffb996b5c7b447fe8ac6ce1f73b47
omaddam <omaddam@gmail.com>
2020-12-31 00:21:41 -0700

    Implemented the UI manager.

--- Assets/App/Scripts/PathTraversalUIManager.cs
+++ Assets/App/Scripts/PathTraversalUIManager.cs
@@ -1,10 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class PathTraversalUIManager : MonoBehaviour
 {
+
+    #region Initialization
+
+    /// <summary>
+    /// Executes once on start.
+    /// </summary>
+    private void Awake()
+    {
+        StartShortestPathButton.onClick.AddListener(() => StartTraversing(true));
+        StartRhumbPathButton.onClick.AddListener(() => StartTraversing(false));
+        CancelButton.onClick.AddListener(Cancel);
+    }
+
+    #endregion
+
+    #region Events
+
+    /// <summary>
+    /// Triggers when the user wants to traverse the shortest path.
+    /// </summary>
+    public UnityEvent OnShortestPathStart;
+
+    /// <summary>
+    /// Triggers when the user wants to traverse the rhumb path.
+    /// </summary>
+    public UnityEvent OnRhumbPathStart;
+
+    /// <summary>
+    /// Triggers when the user wants to cancel.
+    /// </summary>
+    public UnityEvent OnCancel;
+
+    #endregion
 
     #region Fields/Properties
 
@@ -42,6 +76,61 @@ public class PathTraversalUIManager : MonoBehaviour
     [Tooltip("References the cancel button.")]
     [SerializeField]
     private Button CancelButton;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Shows the teaser panel.
+    /// </summary>
+    public void Show()
+    {
+        // Show teaser panel
+        TeaserPanel.SetActive(true);
+
+        // Hide compass panel
+        CompassPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Hides all the panels.
+    /// </summary>
+    public void Hide()
+    {
+        // Hide teaser panel
+        TeaserPanel.SetActive(false);
+
+        // Hide compass panel
+        CompassPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Starts traversing.
+    /// </summary>
+    private void StartTraversing(bool shortestPath)
+    {
+        // Hide teaser panel
+        TeaserPanel.SetActive(false);
+
+        // Show compass panel
+        CompassPanel.SetActive(true);
+
+        // Inform listeners
+        if (shortestPath)
+            OnShortestPathStart.Invoke();
+        else
+            OnRhumbPathStart.Invoke();
+    }
+
+    /// <summary>
+    /// Cancels the traversing.
+    /// </summary>
+    private void Cancel()
+    {
+        // Inform listeners
+        OnCancel.Invoke();
+    }
 
     #endregion
 
