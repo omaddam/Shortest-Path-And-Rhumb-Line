a694b621e8984de32360e5f3e11b03a9cbf9e6cd
omaddam <omaddam@gmail.com>
2020-12-30 18:19:43 -0700

    Added a reference to the camera that will traverse the path.

--- Assets/App/Scripts/PathTraversalManager.cs
+++ Assets/App/Scripts/PathTraversalManager.cs
@@ -5,6 +5,29 @@ using UnityEngine;
 public class PathTraversalManager : MonoBehaviour
 {
 
+    #region Initialization
 
+    /// <summary>
+    /// Executes once on start.
+    /// </summary>
+    private void Awake()
+    {
+        // Seta a default camera
+        if (Camera == null)
+            Camera = Camera.main;
+    }
+
+    #endregion
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// References the camera that will traverse the path.
+    /// </summary>
+    [Tooltip("References the camera that will traverse the path. By default, it references the main camera.")]
+    [SerializeField]
+    private Camera Camera;
+
+    #endregion
 
 }
